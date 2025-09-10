4358159c814718ae5bd7e69db9b83a3f229b1d01
Xenostrike78 <arbaaz9416@gmail.com>
2025-09-10 15:59:50 +0530

    API Added

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -4,27 +4,27 @@ import { title } from "process";
 const router = Router()
 router.get("/",(req,res)=>{
     const content = {
-        title:'FontVerse | Font Combinations'
+        title:'FontVerse | Font Combinations',currentUrl:req.path
     }
     res.render("index",content);
 });
 
 router.get("/how-it-works",(req,res)=>{
     const content = {
-        title:"How It Works | FontVerse"
+        title:"How It Works | FontVerse",currentUrl:req.path
     }
     res.render("how_it_works",content);
 });
 router.get("/gallery",(req,res)=>{
     const content = {
-        title:"Font Gallery | FontVerse"
+        title:"Font Gallery | FontVerse",currentUrl:req.path
     }
     res.render("gallery",content);
 });
 
 router.use("/technicality",(req,res)=>{
     const content = {
-        title:"Technicality  | FontVerse"
+        title:"Technicality  | FontVerse",currentUrl:req.path
     }
     res.render("technicality",content);
 });
